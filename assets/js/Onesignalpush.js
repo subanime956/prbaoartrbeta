afb4c992794000198578390025da023144b45730
m.AnimeRTR <subanime956@gmail.com>
2026-04-05 08:58:54 -0500

    Create Onesignalpush.js

---
+++ assets/js/Onesignalpush.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "70b7d7f2-2753-48cc-ab93-202e2195d76f",
+    });
+  });
+</script>
