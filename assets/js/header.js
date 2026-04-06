1f1e2ec10a4902c6df91b64ca5623bd9350fd1ec
m.AnimeRTR <subanime956@gmail.com>
2026-04-05 19:12:52 -0500

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -29,3 +29,94 @@ document.getElementById("searchInput").addEventListener("keydown", function(e){
     }
   }
 });
+
+
+
+
+const btn = document.getElementById("notifToggle");
+const icon = document.getElementById("bellIcon");
+
+const warning = document.getElementById("notifWarning");
+const closeWarning = document.getElementById("closeWarning");
+
+const toast = document.getElementById("notifToast");
+const toastText = document.getElementById("toastText");
+const closeToast = document.getElementById("closeToast");
+
+let toastTimeout;
+
+// estado inicial
+if (Notification.permission === "granted") {
+  icon.className = "fa fa-bell";
+  btn.classList.add("active");
+} else {
+  icon.className = "fa fa-bell-slash";
+  btn.classList.add("inactive");
+}
+
+function updateUI(state){
+  if(state){
+    icon.className = "fa fa-bell";
+    btn.classList.add("active");
+    btn.classList.remove("inactive");
+  } else {
+    icon.className = "fa fa-bell-slash";
+    btn.classList.add("inactive");
+    btn.classList.remove("active");
+  }
+}
+
+function showToast(text){
+  warning.style.display = "none";
+  toastText.textContent = text;
+  toast.style.display = "block";
+
+  clearTimeout(toastTimeout);
+  toastTimeout = setTimeout(() => {
+    toast.style.display = "none";
+  }, 2000);
+}
+
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+
+OneSignalDeferred.push(async function(OneSignal){
+
+  function sync(){
+    updateUI(OneSignal.User.PushSubscription.optedIn);
+  }
+
+  setTimeout(sync, 100);
+
+  btn.addEventListener("click", async function(){
+
+    const permission = Notification.permission;
+
+    if(permission === "denied"){
+      toast.style.display = "none";
+      warning.style.display = "block";
+      return;
+    }
+
+    const opted = OneSignal.User.PushSubscription.optedIn;
+
+    if(opted){
+      await OneSignal.User.PushSubscription.optOut();
+      updateUI(false);
+      showToast("🔕 Desactivaste las notificaciones");
+    } else {
+
+      if(permission === "default"){
+        await OneSignal.Notifications.requestPermission();
+      } else {
+        await OneSignal.User.PushSubscription.optIn();
+      }
+
+      updateUI(true);
+      showToast("🔔 Activaste las notificaciones");
+    }
+  });
+
+  closeWarning.onclick = () => warning.style.display = "none";
+  closeToast.onclick = () => toast.style.display = "none";
+
+});
